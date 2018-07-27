5802254c503f88c507732ff6703a2457bc270e6d
ajayvadlavalli <41731252+ajayvadlavalli@users.noreply.github.com>
2018-07-27 12:43:57 +0530

    Create large.java

---
+++ large.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int x, y, z;
+      System.out.println("Enter three integers ");
+      Scanner in = new Scanner(System.in);
+ 
+      x = in.nextInt();
+      y = in.nextInt();
+      z = in.nextInt();
+ 
+      if ( x > y && x > z )
+         System.out.println(""+x);
+      else if ( y > x && y > z )
+         System.out.println(""+y);
+      else if ( z > x && z > y )
+         System.out.println(""+z);
+      else   
+         System.out.println("Entered numbers are not distinct.");
+	}
+}
