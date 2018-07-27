780d3455ff06198c559a04f673dc22aa95867f26
ajayvadlavalli <41731252+ajayvadlavalli@users.noreply.github.com>
2018-07-27 12:49:11 +0530

    Update large.java

--- large.java
+++ large.java
@@ -10,7 +10,6 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	    int x, y, z;
-      System.out.println("Enter three integers ");
       Scanner in = new Scanner(System.in);
  
       x = in.nextInt();
