5e9332cc474574beee19c201d80dd9cd91349f77
ajayvadlavalli <41731252+ajayvadlavalli@users.noreply.github.com>
2018-07-27 12:24:43 +0530

    Create oe.java

---
+++ oe.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int a;
+	  Scanner in=new Scanner(System.in);
+	  a=in.nextInt();
+	  if(a>0)
+	  {
+	  	if(a%2==0)
+	  	{
+	  		System.out.println("Even");
+	  		
+	  	}
+	  	else 
+	  	{
+	  		System.out.println("Odd");
+	  		
+	  	}
+	  }	
+	  	else
+	  	{
+	  		System.out.println("Invalid");
+	      }
+	
+}
+}
